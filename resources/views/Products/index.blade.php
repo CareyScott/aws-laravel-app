77f9e2355ebec53d961ea7ed606901de8a0eb845
CareyScott <yeracsott9>
2023-06-21 19:14:40 +0100

    Bug Fixing - Products was not capitalised

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -7,7 +7,7 @@
                 <h2>Laravel Development</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('Products.create') }}"> Create new Product</a>
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create new Product</a>
             </div>
         </div>
     </div>
