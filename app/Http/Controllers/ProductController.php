5d977148530cb9087006dc4c19f6234e61a19036
CareyScott <yeracsott9>
2023-06-21 20:04:26 +0100

    Bug Fixing - Products was not capitalised

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
         //showing list of products
         $products = Product::latest()->paginate(5);
 
-        return view('products.index', compact('products'))->with(request()->input('page'));
+        return view('Products.index', compact('products'))->with(request()->input('page'));
     }
 
     /**
@@ -23,7 +23,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('products.create');
+        return view('Products.create');
     }
 
     /**
@@ -57,7 +57,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        return view('products.edit', compact('product'));
+        return view('Products.edit', compact('product'));
     }
 
     /**
@@ -75,7 +75,7 @@ class ProductController extends Controller
         $product->update($request->all());
 
         //redirect user and sent message of success
-        return redirect()->route('products.index')->with('success', 'product updated successfully');
+        return redirect()->route('Products.index')->with('success', 'product updated successfully');
     }
 
     /**
@@ -87,7 +87,7 @@ class ProductController extends Controller
         $product->delete();
 
         // redirect the user & display success message
-        return redirect()->route('products.index')->with('success', 'product deleted successfully');
+        return redirect()->route('Products.index')->with('success', 'product deleted successfully');
 
     }
 }
