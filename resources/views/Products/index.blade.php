2a5bcc3a41081917c0cef19fc854b575ee17c4ee
CareyScott <yeracsott9>
2023-06-21 19:15:44 +0100

    Bug Fixing - Products was not capitalised

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -32,9 +32,9 @@
                 <td>{{ $product->name }}</td>
                 <td>{{ $product->detail }}</td>
                 <td>
-                    <form action="{{ route('Products.destroy', $product->id) }}" method="post">
-                        <a class="btn btn-info" href="{{ route('Products.show', $product->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('Products.edit', $product->id) }}">Edit</a>
+                    <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                        <a class="btn btn-info" href="{{ route('products.show', $product->id) }}">Show</a>
+                        <a class="btn btn-primary" href="{{ route('products.edit', $product->id) }}">Edit</a>
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">Delete</button>
